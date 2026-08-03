9244639cb534940efdd691aa49406159c450f82f
shivmtripathi <41698028+shivmtripathi@users.noreply.github.com>
2026-08-03 21:53:24 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.java
@@ -0,0 +1,48 @@
+class MyHashSet {
+
+    private List<Integer>[] buckets;
+    private static final int SIZE = 1000;
+
+    public MyHashSet() {
+        this.buckets = new List[1000];
+        for (int i = 0; i < SIZE; i++) {
+        buckets[i] = new ArrayList<>();
+    }
+        
+    }
+    
+    public void add(int key) {
+
+        int bucket = key % SIZE;
+        List<Integer> list = buckets[bucket];
+        if(!list.contains(key)){
+            list.add(key);
+        }        
+    }
+    
+    public void remove(int key) {
+        int bucket = key % SIZE;
+        List<Integer> list = buckets[bucket];
+        if(list.contains(key)){
+         list.remove(Integer.valueOf(key));
+
+    }
+
+        
+    }
+    
+    public boolean contains(int key) {
+                int bucket = key % SIZE;
+        List<Integer> list = buckets[bucket];
+               return list.contains(key);
+        
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
